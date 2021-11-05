bcf00914df2deff94f21a96408b483a5507e8134
Hufan <hufany21@gmail.com>
2021-11-05 08:56:24 -0400

    refac

--- breedFetcher.js
+++ breedFetcher.js
@@ -2,13 +2,16 @@ const request = require('request');
 const breedName = process.argv.slice(2);
 
 
+const fetchBreedDescription = function(breedName, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+    const data = JSON.parse(body);
+    const description = data.length > 0 ? data[0]['description'] : null;
+    
+    
+    
+    callback(error, description);
+  });
+};
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
-  if (body === '[]') {
-    console.log('Error, this breed was not found');
-    return;
-  }
-  const data = JSON.parse(body);
-  console.log(data[0]['description']);
 
-});
+module.exports = { fetchBreedDescription };
